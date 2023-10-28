d505028ab1144b929799bda08214a4375a228cef
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2023-10-28 20:24:06 +0700

    Membuat Migration PK FK Table

---
+++ database/migrations/2023_10_28_130227_create_detail_pengambilan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_pengambilan', function (Blueprint $table) {
+            $table->bigIncrements('id_dtl_pengambilan');
+
+            $table->unsignedBigInteger('id_dtl_langganan');
+            $table->foreign('id_dtl_langganan')->references('id_dtl_langganan')->on('detail_langganan');
+
+            $table->unsignedBigInteger('id_pengambilsampah');
+            $table->foreign('id_pengambilsampah')->references('id_pengambilsampah')->on('pengambil_sampah');
+
+            $table->date('tanggal');
+            $table->string('hari', 12);
+            $table->integer('berat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_pengambilan');
+    }
+};
---
+++ database/migrations/2023_10_28_130742_create_penerimaan_sampah_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penerimaan_sampah', function (Blueprint $table) {
+            $table->bigIncrements('id_penerimaan_sampah');
+
+            $table->char('kode_penerimaan', 5);
+            $table->foreign('kode_penerimaan')->references('kode_penerimaan')->on('status_penerimaan');
+
+            $table->unsignedBigInteger('id_banksampah');
+            $table->foreign('id_banksampah')->references('id_banksampah')->on('bank_sampah');
+
+            $table->unsignedBigInteger('id_dtl_pengambilan');
+            $table->foreign('id_dtl_pengambilan')->references('id_dtl_pengambilan')->on('detail_pengambilan');
+
+            $table->time('jam_terima');
+            $table->date('tanggal_terima');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penerimaan_sampah');
+    }
+};
