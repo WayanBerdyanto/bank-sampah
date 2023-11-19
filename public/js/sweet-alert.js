37ce28020cac9988b67b45e3a1001d7d09fcba9d
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2023-11-19 20:39:16 +0700

    Delete SweetAlert in public/js

--- public/js/sweet-alert.js
+++
@@ -1,9 +0,0 @@
-import Swal from "sweetalert2";
-
-const Swal = require('sweetalert2')
-Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool'
-  })
