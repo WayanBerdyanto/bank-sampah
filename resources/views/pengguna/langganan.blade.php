bab398f09a6cc218619ba07b9336156f32cb8e81
Wayan Berdyanto <aber.panak.atok@gmail.com>
2023-12-02 20:52:40 +0700

    Update Db Langganan

--- resources/views/pengguna/langganan.blade.php
+++ resources/views/pengguna/langganan.blade.php
@@ -4,31 +4,28 @@
         <div class="container-fluid">
             <div class="row mt-5 mx-3 mb-3">
                 @foreach ($langganan as $data)
-                    <div class="col-md-3 mb-3">
+                    <div class="col-md-6 mb-3">
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title fw-bold fs-5">{{$data->nama_langganan}}</h5>
+                                <h5 class="card-title fw-bold fs-5">{{ $data->nama_langganan }}</h5>
+                                @php
+                                    $items = explode(',', $data->layanan);
+                                @endphp
                                 <ul>
-                                    <li>
-                                        <i class="bi bi-check-lg text-success"></i>
-                                        berat
-                                    </li>
-                                    <li>
-                                        <i class="bi bi-check-lg text-success"></i>
-                                        berat
-                                    </li>
-                                    <li>
-                                        <i class="bi bi-check-lg text-success"></i>
-                                        berat
-                                    </li>
+                                    @foreach ($items as $item)
+                                        <li>
+                                            <i class="bi bi-check-lg text-success"></i>
+                                            {{ trim($item) }}
+                                        </li>
+                                    @endforeach
                                 </ul>
                                 <div class="d-flex justify-content-between mt-2">
                                     <span>Rp
                                         <span class="fs-5 fw-bold">
-                                            {{$data->harga}}
+                                            {{ $data->harga }}
                                         </span>
                                     </span>
-                                    <a href="#" class="btn btn-primary">Beli</a>
+                                    <a href="/langganan/{{$data->kode_langganan}}" class="btn btn-primary">Beli</a>
                                 </div>
                             </div>
                         </div>
