3c74d7438bda3f857fb4f7d273d4bb7229305911
Wayan Berdyanto <aber.panak.atok@gmail.com>
2023-12-05 20:50:58 +0700

    Update

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -111,7 +111,7 @@ class PenggunaController extends Controller
 
         $params = array(
             'transaction_details' => array(
-                'order_id' => $order->id_dtl_langganan, //<- Manipulation Callback
+                'order_id' => $order->id_dtl_langganan . '' . uniqid(), //<- Manipulation Callback
                 'gross_amount' => $order->harga,
             ),
             'customer_details' => array(
