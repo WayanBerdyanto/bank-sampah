d0d3a44412ae7ec123c34c7555316b873913fc48
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2023-12-13 18:42:32 +0700

    update design dashboard pengambil

--- resources/views/pengambil/index.blade.php
+++ resources/views/pengambil/index.blade.php
@@ -4,144 +4,85 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h4>Dashboard Pengambil</h4>
+                    <h4 class="my-3 text-center">Dashboard Pengambil</h4>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-3 mb-3">
+                <div class="col-md-4 mb-3">
                     <div class="card bg-primary text-white h-100">
-                        <div class="card-body py-5">Primary Card</div>
-                        <div class="card-footer d-flex">
-                            View Details
-                            <span class="ms-auto">
-                                <i class="bi bi-chevron-right"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                    <div class="card bg-warning text-dark h-100">
-                        <div class="card-body py-5">Warning Card</div>
-                        <div class="card-footer d-flex">
-                            View Details
-                            <span class="ms-auto">
-                                <i class="bi bi-chevron-right"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                    <div class="card bg-success text-white h-100">
-                        <div class="card-body py-5">Success Card</div>
-                        <div class="card-footer d-flex">
-                            View Details
-                            <span class="ms-auto">
-                                <i class="bi bi-chevron-right"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                    <div class="card bg-danger text-white h-100">
-                        <div class="card-body py-5">Danger Card</div>
-                        <div class="card-footer d-flex">
-                            View Details
-                            <span class="ms-auto">
-                                <i class="bi bi-chevron-right"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 mb-3">
-                    <div class="card h-100">
                         <div class="card-header">
-                            <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
-                            Area Chart Example
+                            Data Request Pembuangan
                         </div>
-                        <div class="card-body">
-                            <canvas class="chart" width="400" height="200"></canvas>
+                        <div class="card-body py-5">
+                            50
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <div class="card h-100">
-                        <div class="card-header">
-                            <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
-                            Area Chart Example
-                        </div>
-                        <div class="card-body">
-                            <canvas class="chart" width="400" height="200"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 mb-3">
-                    <div class="card">
-                        <div class="card-header">
-                            <span><i class="bi bi-table me-2"></i></span> Data Table
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="example" class="table table-striped data-table" style="width: 100%">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Garrett Winters</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            <td>$170,750</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ashton Cox</td>
-                                            <td>Junior Technical Author</td>
-                                            <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12</td>
-                                            <td>$86,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Cedric Kelly</td>
-                                            <td>Senior Javascript Developer</td>
-                                            <td>Edinburgh</td>
-                                            <td>22</td>
-                                            <td>2012/03/29</td>
-                                            <td>$433,060</td>
-                                        </tr>
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot>
-                                </table>
+                        <a href="/pengambil/requestpembuangan" class="btn text-light">
+                            <div class="card-footer d-flex">
+                                View Details
+                                <span class="ms-auto">
+                                    <i class="bi bi-chevron-right"></i>
+                                </span>
                             </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card bg-success text-white h-100">
+                        <div class="card-header">
+                            Sampah yang belum Diambil
+                        </div>
+                        <div class="card-body py-5">
+                            <span>Success Card</span>
+                        </div>
+                        <a href="/pengambil/penerimaan" class="btn text-light">
+                            <div class="card-footer d-flex">
+                                View Details
+                                <span class="ms-auto">
+                                    <i class="bi bi-chevron-right"></i>
+                                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card bg-danger text-white h-100">
+                        <div class="card-header">
+                            Data History
+                        </div>
+                        <div class="card-body py-5">
+                            <span>Danger Card</span>
+                        </div>
+                        <a href="/pengambil/history" class="btn text-light">
+                            <div class="card-footer d-flex">
+                                View Details
+                                <span class="ms-auto">
+                                    <i class="bi bi-chevron-right"></i>
+                                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-md-6 mb-3">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
+                            Area Chart Example
+                        </div>
+                        <div class="card-body">
+                            <canvas class="chart" width="400" height="200"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
+                            Area Chart Example
+                        </div>
+                        <div class="card-body">
+                            <canvas class="chart" width="400" height="200"></canvas>
                         </div>
                     </div>
                 </div>
