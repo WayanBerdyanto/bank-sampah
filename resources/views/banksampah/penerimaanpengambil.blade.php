5cc50e97687d5a00347db942fcab460b690ecd06
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2023-12-13 15:57:06 +0700

    Update view Data Penerimaan Sampah (banksampah)

---
+++ resources/views/banksampah/penerimaanpengambil.blade.php
@@ -0,0 +1,27 @@
+<h4 class="my-3 text-center">Data Penerimaan Sampah Pengambil <i class="bi bi-receipt"></i></h4>
+<div class="card mx-4">
+    <div class="card-header">
+        <span><i class="bi bi-table me-2"></i></span> Data Penerimaan Sampah
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table id="example" class="table table-striped data-table" style="width: 100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Pengambil</th>
+                        <th>Berat Sampah</th>
+                        <th>Tgl Pengajuan</th>
+                        <th>Jam Pengajuan</th>
+                        <th class="text-center">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
