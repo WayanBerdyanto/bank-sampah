152593c00c42c72f1c384b098a625265ce447e3f
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2023-12-13 16:37:26 +0700

    Add Data in view Requestpembuangan

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Models\master_pengambilan;
+
+
 
 class RequestController extends Controller
 {
     public function requestPembuangan(){
-        return view('pengambil.requestpembuaganpage');
+        $getBank = user::where('role', 'banksampah')->get();
+
+        $pengambil = Auth::user()->id;
+        $result = master_pengambilan::join('detail_pengambilan as dp', 'master_pengambilan.id_nota', '=', 'dp.id_nota')
+        ->join('users as us', 'master_pengambilan.id_pengguna', '=', 'us.id')
+        ->where('dp.id_pengambil', '=', $pengambil)
+        ->select(
+            'master_pengambilan.id_pengguna',
+            'dp.id_dtl_pengambilan',
+            'dp.id_nota',
+            'dp.id_pengambil',
+            'master_pengambilan.jenis_sampah',
+            'us.nama_lengkap',
+            'dp.status_pengambilan',
+            'dp.berat'
+        )
+        ->orderBy('master_pengambilan.tanggal', 'desc')
+        ->orderBy('master_pengambilan.jam', 'desc')
+        ->paginate(10);
+
+        return view('pengambil.requestpembuaganpage', ['getBank'=>$getBank, 'result'=>$result]);
     }
 }
--- resources/views/pengambil/requestpembuaganpage.blade.php
+++ resources/views/pengambil/requestpembuaganpage.blade.php
@@ -5,9 +5,137 @@
             <div class="row">
                 <h4 class="mx-4 my-3 text-center">Data Request buang Sampah</h4>
                 <div class="col-md-12 mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <span><i class="bi bi-table me-2"></i></span> Data Penerimaan Sampah
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="example" class="table table-striped data-table" style="width: 100%">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Pengguna</th>
+                                            <th>Jenis Sampah</th>
+                                            <th>Berat Sampah</th>
+                                            <th>Lokasi Pembuangan</th>
+                                            <th class="text-center">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($result as $idx => $item)
+                                            <tr>
+                                                <td> {{$loop->index + 1 }} </td>
+                                                <td>{{$item->nama_lengkap}}</td>
+                                                <td>
+                                                    {{$item->jenis_sampah}}
+                                                </td>
+                                                <td class="text-center">
+                                                    {{$item->berat}} Kg
+                                                </td>
+                                                <form action="" method="POST">
+                                                    @csrf
+                                                    <td hidden>
+                                                        <input type="text" name="id_dtl_pengambilan" value="{{$item->id_dtl_pengambilan}}">
+                                                    </td>
+                                                    <td>
+                                                        <select class="form-select" name="idbanksampah"
+                                                            aria-label="Default select example" required>
+                                                            <option selected>Pilih Lokasi Pembuangan</option>
+                                                            @foreach ($getBank as $data)
+                                                            
+                                                                <option value="{{ $data->id }}">
+                                                                    {{ $data->nama_lengkap }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </td>
+                                                    <td>
+                                                        <button type="submit" class="btn btn-primary">Request</button>
+                                                    </td>
+                                                </form>
 
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <span class="mr-2 page-link pagination">
+                                    {{ $result->onEachSide(10)->links() }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mb-3">
+                        <h4 class="form-label fw-bold my-3">Lokasi Banksampah</h4>
+                        <div class="card-body" id="mapss" style="height: 400px"></div>
+                        <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+
+                        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+                            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+                        <script>
+                            var map = L.map('mapss').setView([0, 0], 30);
+                            // Add a basemap (e.g., OpenStreetMap)
+                            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                maxZoom: 19,
+                                attribution: '&copy; <ahref = "http://www.openstreetmap.org/copyright" > OpenStreetMap < /a>'
+                            }).addTo(map);
+
+                            // Get the user's geolocation and add a marker
+                            navigator.geolocation.getCurrentPosition(function(position) {
+                                var lat = position.coords.latitude;
+                                var lon = position.coords.longitude;
+                                map.setView([lat, lon], 10);
+                                var userLocation = L.marker([lat, lon]).addTo(map);
+                                userLocation.bindPopup('Lokasi Anda saat ini').openPopup();
+                            });
+                            var iconUrls = [
+                                'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                                'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+                                'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+                                'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+                                'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-purple.png',
+                                'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+                            ];
+
+
+                            var locations = <?php echo json_encode($getBank); ?>;
+                            locations.forEach(e => {
+                                var randomIconUrl = iconUrls[Math.floor(Math.random() * iconUrls.length)];
+
+                                console.log(e.latitude);
+                                console.log(e.longitude);
+                                console.log(e.nama_lengkap);
+                                console.log(e.kabupaten);
+
+                                var greenIcon = new L.Icon({
+                                    iconUrl: randomIconUrl,
+                                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                                    iconSize: [25, 41],
+                                    iconAnchor: [12, 41],
+                                    popupAnchor: [1, -34],
+                                    shadowSize: [41, 41]
+                                });
+
+                                // Creating a marker for each bank sampah location with default icon and random color
+                                var marker = L.marker([e.latitude, e.longitude], {
+                                    icon: greenIcon
+                                }).addTo(map);
+
+                                // Creating a popup with information and attaching it to the marker
+                                var popupContent = "<b>" + e.nama_lengkap + "</b><br>Penampungan: " + e.kapasitas + " Kg";
+                                marker.bindPopup(popupContent).openPopup();
+                            });
+                        </script>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     </main>
 @endsection
