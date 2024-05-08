8358cd172068cbf97d36a1c9b9b738234428e636
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2024-05-08 16:43:14 +0700

    Update Register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
-
 use App\Models\User;
 
 class RegisterController extends Controller
@@ -15,18 +13,23 @@ class RegisterController extends Controller
     }
 
     public function postRegister(Request $request){
-        if($request->password == $request->password2){
-            User::create([
-                'username'=> $request->username,
-                'nama_lengkap'=> $request->nama_lengkap,
-                'email'=> $request->email,
-                'password'=> bcrypt($request->password),
-                'no_telpon'=> $request->no_telpon
-            ]);
-            return redirect('/login')->with('success', 'Register Berhasil');
-        }
-        else{
+        $user = User::where('username', $request->username)->first();
+        if($user != null){
             return redirect('/register')->with('errors', 'Data Yang Di Inputkan Salah')->withInput();
+        }else{
+            if($request->password == $request->password2){
+                User::create([
+                    'username'=> $request->username,
+                    'nama_lengkap'=> $request->nama_lengkap,
+                    'email'=> $request->email,
+                    'password'=> bcrypt($request->password),
+                    'no_telpon'=> $request->no_telpon
+                ]);
+                return redirect('/login')->with('success', 'Register Berhasil');
+            }
+            else{
+                return redirect('/register')->with('errors', 'Data Yang Di Inputkan Salah')->withInput();
+            }
         }
     }
 }
--- migrate.bat
+++ migrate.bat
@@ -1,6 +1,6 @@
 @echo off
 set "php_path=C:\laragon\bin\php\php-8.1.10-Win32-vs16-x64\php.exe"  REM Ganti dengan path PHP yang sesuai
-set "artisan_path=C:\Users\USER\OneDrive\Desktop\bank-sampah" REM Ganti dengan path proyek Laravel Anda
+set "artisan_path=C:\laragon\www\bank-sampah" REM Ganti dengan path proyek Laravel Anda
 
 cd %artisan_path%
 
